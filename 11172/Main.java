4af903c295027de7f51db3a4892923f5c0d641e6
WongTszLai <wongtszlai@hotmail.com>
2013-09-14 17:24:42 +0800

    WTF! an additional space after the answer gave "Presentation Error" message!
    Such picky judges...

---
+++ 11172/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int holder1, holder2;
+		int lines = in.nextInt();
+
+		while (0 != lines--) {
+			holder1 = Integer.parseInt(in.next());
+			holder2 = Integer.parseInt(in.next());
+			if (holder1 > holder2) {
+				out.print(">\n");
+			}
+			else if (holder1 < holder2) {
+				out.print("<\n");
+			}
+			else {
+				out.print("=\n");
+			}
+			
+		}
+	}
+}
